1d7a0be155eaa6bd2772f0652eaf0371f6575820
Raj Soni <rajksoni029@gmail.com>
2021-11-29 11:06:52 +0530

    heroku setup updated

--- SafeZone/settings.py
+++ SafeZone/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
