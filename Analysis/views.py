789385ba79e8320f0801713cc3d6426e11771e1f
Raj Soni <rajksoni029@gmail.com>
2021-11-30 11:21:55 +0530

    analysis update

--- Analysis/views.py
+++ Analysis/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def dataInput(request):
+	return render(request, "Analysis/dataInput.html")
+
+
+def dashboard(request):
+	return render(request, "Analysis/dashboard.html")
