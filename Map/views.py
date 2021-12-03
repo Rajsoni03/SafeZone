f61ea79eb2170719c35059600c4af125ad3a4177
Rajkumar Soni <rajksoni029@gmail.com>
2021-12-03 11:57:09 +0530

    map updated

--- Map/views.py
+++ Map/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from DataEntry.models import Crime
 
 # Create your views here.
 def map(request):
-	return render(request, "Map/map.html")
+	crimes = Crime.objects.all()
+	lat_long = []
+	long_sum = 0
+	lat_sum = 0
+	num = 0
+	for crime in crimes:
+		lat_long.append([float(crime.longitude), float(crime.latitude)])
+
+		if num > 100:
+			break
+		num = num + 1
+		long_sum = long_sum + float(crime.longitude)
+		lat_sum = lat_sum + float(crime.latitude)
+
+	total_len = len(lat_long)
+	params = {
+		'lat_long' : lat_long,
+		# 'center' : [(long_sum/total_len), (lat_sum/total_len)],
+		'center' : lat_long[0],
+		'first_point': lat_long[-1],
+		'map_zoom' : 11,
+	}
+	return render(request, "Map/map.html", params)
