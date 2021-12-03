eabcbe875ccefd09186ccf8e6a01f7ffcf37b9fe
Rajkumar Soni <rajksoni029@gmail.com>
2021-12-03 14:38:44 +0530

    Map Updated

--- Map/views.py
+++ Map/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from DataEntry.models import Crime
+from django.http import JsonResponse
 
 # Create your views here.
 def map(request):
@@ -11,7 +12,7 @@ def map(request):
 	for crime in crimes:
 		lat_long.append([float(crime.longitude), float(crime.latitude)])
 
-		if num > 100:
+		if num > 5:
 			break
 		num = num + 1
 		long_sum = long_sum + float(crime.longitude)
@@ -22,7 +23,22 @@ def map(request):
 		'lat_long' : lat_long,
 		# 'center' : [(long_sum/total_len), (lat_sum/total_len)],
 		'center' : lat_long[0],
-		'first_point': lat_long[-1],
-		'map_zoom' : 11,
+		'first_point': lat_long[-5],
+		'map_zoom' : 13,
 	}
 	return render(request, "Map/map.html", params)
+
+def dataPoints(request):
+	head = '{"type": "FeatureCollection","features": ['
+	tail = ']}'
+	data_head = '{"geometry": { "type": "Point", "coordinates": '
+	data_tail = '} },'
+
+	crimes = Crime.objects.all()
+	lat_long = ''
+
+	for crime in crimes:
+		lat_long = lat_long + data_head + str([float(crime.longitude), float(crime.latitude)]) + data_tail
+
+	data = head + lat_long + tail
+	return JsonResponse(data=eval(data))
