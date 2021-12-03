41c605bbdbf719845304d559144fbf5ff2ff5ec8
Sachin Gupta <sgupta15120@gmail.com>
2021-12-03 12:19:04 +0530

    changes update

--- Data Analysis/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from pandas_profiling import ProfileReport
-
-df = pd.read_excel('PS7 predictive policing sample data (1).xlsx')
-print(df)
-
-# Generate a report
-profile = ProfileReport(df)
-profile.to_file(output_file="PS7.html")
